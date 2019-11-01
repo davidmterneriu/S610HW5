973b810f58b316a6cdbd3220325a946c24aca8cc
David Terner <davidmterner@gmail.com>
2019-10-31 21:50:39 -0400

    Initial commit for this homework

---
+++ benchmark_llr.R
@@ -0,0 +1,14 @@
+#benchmark_llr.R
+library(microbenchmark)
+library(testthat)
+
+source("llr_functions.R")
+
+n = 100
+## a very simple regression model
+set.seed(123)
+x = rnorm(n)
+y = rnorm(x + rnorm(n))
+z = seq(-1, 1, length.out = 100)
+
+microbenchmark::microbenchmark(llr(x, y, z, omega = 1))%>%print()
